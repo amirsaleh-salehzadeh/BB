376169d99df1d36c10a6d7b14635ae7743bcd9f8
amirsaleh-salehzadeh <salehzadeh@gmail.com>
2018-07-16 11:02:22 +0200

    cc

--- MSP/WebContent/extension/python/muse_connection.py
+++ MSP/WebContent/extension/python/muse_connection.py
@@ -4,41 +4,47 @@ import time
 from matplotlib.backends.backend_webagg import ServerThread
 from numpy.testing.nose_tools.parameterized import make_method
 
-from liblo import *
 from builtins import Exception
+from numpy import str
 
 
 class MuseServer(ServerThread):
-    #listen for messages on port 5000
+    acc_x, acc_y, acc_z = .0, .0, .0
+    EEG1, EEG2 = .0, .0
+    hs1, hs2, hs3, hs4 = .4, .4, .4, .4
+    isRecord = False
     def __init__(self):
-        ServerThread.__init__(self, 5000)
+        ServerThread.__init__(self, 5003)
 
-    #receive accelrometer data
     @make_method('/muse/acc', 'fff')
     def acc_callback(self, path, args):
         acc_x, acc_y, acc_z = args
-        print (path, acc_x, acc_y, acc_z)
+        
+    @make_method('/muse/horseshoe', 'ffff')
+    def acc_callback(self, path, args):
+        hs1, hs2, hs3, hs4 = args
+        print (hs1, hs2, hs3, hs4)
 
-    #receive EEG data
     @make_method('/muse/eeg', 'ffff')
     def eeg_callback(self, path, args):
-        l_ear, l_forehead, r_forehead, r_ear = args
-        print (path, l_ear, l_forehead, r_forehead, r_ear)
+        l_ear, EEG1, EEG2, r_ear = args
 
-    #handle unexpected messages
     @make_method(None, None)
     def fallback(self, path, args, types, src):
-        print ("Unknown message ",src.url, path, types, args)
+        print ("Unknown message ", src.url, path, types, args)
+        
+    with codecs.open("C:/RecordingFiles/data.csv", 'a') as logfile:
+        writer = csv.DictWriter(logfile)
+        writer.writerow(EEG1, EEG2, acc_x, acc_y, acc_z)
 
 try:
     server = MuseServer()
-except Exception err::
-    print str(err)
+except Exception as err:
+    print (err)
     sys.exit()
-
 
 server.start()
 
 if __name__ == "__main__":
     while 1:
-        time.sleep(1)
+        time.sleep(0.005)
--- MSP/WebContent/extension/python/muse_eeg.py
+++ MSP/WebContent/extension/python/muse_eeg.py
@@ -3,9 +3,28 @@ import argparse
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
+_x, _y, _z = .0, .0, .0
+EEG1, EEG2 = .0, .0
+isRecording = False
 
-def eeg_handler(unused_addr, args, ch1, ch2, ch3, ch4):
-    print("EEG (uV) per channel: ", ch1, ch2, ch3, ch4)
+def acc_handler(unused_addr, args, acc_x, acc_y, acc_z):
+    _x, _y, _z = acc_x, acc_y, acc_z
+
+    
+def hs_handler(unused_addr, args, hs1, hs2, hs3, hs4):
+    _hs2, _hs3 = hs2, hs3
+    if(_hs2 == 1.0 and _hs3 == 1.0):
+        isRecording = True
+    print (hs1, hs2, hs3, hs4)
+
+
+def eeg_handler(unused_addr, args, l_ear, EEG1, EEG2, r_ear):
+    if(isRecording):
+        print(EEG1, EEG2, _x, _y, _z)
+        with codecs.open("C:/RecordingFiles/data.csv", 'a') as logfile:
+            writer = csv.DictWriter(logfile)
+            writer.writerow(EEG1, EEG2, _x, _y, _z)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -14,15 +33,18 @@ if __name__ == "__main__":
                         help="The ip to listen on")
     parser.add_argument("--port",
                         type=int,
-                        default=5000,
+                        default=5003,
                         help="The port to listen on")
     args = parser.parse_args()
 
     dispatcher = dispatcher.Dispatcher()
     dispatcher.map("/debug", print)
     dispatcher.map("/muse/eeg", eeg_handler, "EEG")
+    dispatcher.map("/muse/acc", acc_handler, "ACC")
+    dispatcher.map("/muse/elements/horseshoe", hs_handler, "HSH")
 
     server = osc_server.ThreadingOSCUDPServer(
         (args.ip, args.port), dispatcher)
     print("Serving on {}".format(server.server_address))
     server.serve_forever()
+    
